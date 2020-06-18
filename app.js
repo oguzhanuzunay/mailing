528b7d10950b2dd98971af0fa3f4d1c2956a7370
Oguzhan Uzunay <oguzhanzny@gmail.com>
2020-06-18 07:35:37 +0300

    make it better3

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html; charset=ytf-8' });
-  if(req.method==="GET"){
-    if (req.url==="/" ) {
-        fs.readFile('index.html', (err, data) => {
-            if (err) throw err;
-            res.end(data);
-          });
-    }
-    else if (req.url==='/tool') {
-        fs.readFile('Mailing2.5.3.html', (err, data) => {
-            if (err) throw err;
-            res.end(data);
-          });
-    }
-    else if (req.url==='/adminpage') {
-        fs.readFile('adminPage.html', (err, data) => {
-            if (err) throw err;
-            res.end(data);
-          });
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile('index.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
+    } else if (req.url === '/tool') {
+      fs.readFile('Mailing2.5.3.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
+    } else if (req.url === '/adminpage') {
+      fs.readFile('adminPage.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
     }
   }
 });
 
-server.listen(3001);
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function () {
+  console.log('app running on port 8080');
+});
